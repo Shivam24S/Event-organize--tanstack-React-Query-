c6d5db911a363037ee660282f2d0ce52f264d942
Shivam24S <shivamshrivastav8055@gmail.com>
2024-03-15 15:33:31 +0530

    sending data using mutation

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,25 +1,50 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { createNewEvent } from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  // here we are used useMutation for data sending we can do that using useQuery also
+  // but useMutation has some optimization it gives privilege that data only sent when you want
+  // not when component render instead of when data submitted
+
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: createNewEvent,
+  });
+
+  function handleSubmit(formData) {
+    mutate({ event: formData });
+  }
 
   return (
-    <Modal onClose={() => navigate('../')}>
+    <Modal onClose={() => navigate("../")}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {isPending && <p>Submitting</p>}
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
       </EventForm>
+      {isError && (
+        <ErrorBlock
+          title="failed to create event"
+          message={
+            error.info?.message ||
+            "failed to create event please fill input field correctly"
+          }
+        />
+      )}
     </Modal>
   );
 }
