58d493b177495863aa8a04d199fad67862db55d5
Shivam24S <shivamshrivastav8055@gmail.com>
2024-03-15 20:26:53 +0530

    updating data with mutation

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,27 +1,92 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { fetchEvent, queryClient, updateEvent } from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EditEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  const params = useParams();
 
-  function handleClose() {
-    navigate('../');
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", { id: params.id }],
+    queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
+  });
+
+  const {
+    mutate,
+    isPending: updatePending,
+    isError: updatingError,
+    error: updateError,
+  } = useMutation({
+    mutationFn: updateEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events"] });
+      navigate("/events");
+    },
+  });
+
+  function handleSubmit(formData) {
+    mutate({ id: params.id, event: formData });
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
-      </EventForm>
-    </Modal>
-  );
+  function handleClose() {
+    navigate("../");
+  }
+
+  let content;
+
+  if (isPending) {
+    content = (
+      <div className="center">
+        <LoadingIndicator />
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <>
+        <ErrorBlock
+          title="can't able to load Data"
+          message={
+            error.info?.message || "please try again later after Some time"
+          }
+        />
+        <div className="center">
+          <Link to="..">
+            <button className="button">Okay</button>
+          </Link>
+        </div>
+      </>
+    );
+  }
+
+  if (data) {
+    content = (
+      <>
+        <EventForm inputData={data} onSubmit={handleSubmit}>
+          {isPending && <p> Loading ...</p>}
+          {!isPending && (
+            <>
+              <Link to="../" className="button-text">
+                <button disabled={updatePending} className="button-text">
+                  Cancel
+                </button>
+              </Link>
+              <button type="submit" className="button">
+                {updatePending ? "updating..." : "update"}
+              </button>
+            </>
+          )}
+        </EventForm>
+      </>
+    );
+  }
+
+  return <Modal onClose={handleClose}>{content}</Modal>;
 }
